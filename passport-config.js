684f3b145f1c085d25fdaa6fee3857c76f7e670d
unknown <bpippal@gmail.com>
2023-01-15 17:26:29 +0530

    Added passport-local implementation along with simple ejs renders

---
+++ passport-config.js
@@ -0,0 +1,55 @@
+const Localstrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+
+function init(passport, getUserByEmail, getUserByid){
+
+    console.log("I am init now")
+
+    const authenticateUser = async (email, password, done) => {
+        
+        const user = getUserByEmail(email);
+
+        if(user === (null || undefined)){
+            return done(null , false , {message : "No user with that email"})
+        }
+
+        try {
+            if(await bcrypt.compare(password, user.password)){
+                return done(null , user);
+            }else{
+                return done(null, false, {message : "Incorrect credentials"})
+            }
+        } catch (error) {
+            return done(error);
+        }
+
+    }
+
+    //Below usernameField is the field name which is considered from the payload request
+    // usernameField - Optional, defaults to 'username'
+    // passwordField - Optional, defaults to 'password'
+
+    passport.use(new Localstrategy({usernameField : "email" , passwordField : "password"} , authenticateUser))
+
+    /*
+    The user used in seralizeUser is the ONE THAT IS PASSED IN THE DONE CALLBACK FN LINE-18 OF THIS PAGE AND WE ARE SERIALIZE THE ID PROPERTY OF THE USER 
+    The structure of the user object is - 
+        {
+            id : Date.now().toString(),
+            name : req.body.name,
+            email : req.body.email,
+            password : hashedPassword
+        }
+    */
+    passport.serializeUser((user , done) => done(null , user.id));
+
+    /*
+        Now coming to deseralizing, we are deseralizing what we chose to serialize which is the ID property, hence we pass the id as the first parameter in the deserialaizeFunction below, and the second parameter done, and within it, the done function is supposed to get null as first arg, and the actual userObj as the second arg
+    */
+
+    passport.deserializeUser((id , done) => {
+        done(null , getUserByid(id));
+    });
+}
+
+module.exports = init;
--- server.js
+++ server.js
@@ -1,16 +1,86 @@
+if (process.env.NODE_ENV !== "production"){
+    require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 
+// Storing users at the server level
+const users = [];
+
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride  = require("method-override");
+const initializePassport = require("./passport-config")
+
+initializePassport(passport , 
+    email => users.find(user => user.email === email) , 
+    id => users.find(user => user.id === id)
+)
+
+//Middlewares
+app.set("view-engine", "ejs");
+app.use(express.urlencoded({extended : false}))
+app.use(flash());
+app.use(session({
+    secret : process.env.SESSION_SECRET,
+    resave : false,
+    saveUninitialized : false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"))
+
+//Logger function to see req.session on every request
+// app.use("*" , function(req, res , next){
+//     console.log("Main middleware always called");
+//     console.log(req.session);
+
+//     return next();
+// })
+
+//Get methods to render respectivie page's
+app.get("/" , (req ,res) => {
+    res.render("index.ejs" , {name : "Bharat"});
+})
+
+app.get("/login" , (req, res) => {
+    res.render("login.ejs");
+})
+
+app.get("/register" , (req, res) => {
+    res.render("register.ejs");
+})
 
 
+//Post methods to initiate the login/register
+app.post("/login" , passport.authenticate("local" , {
+    failureRedirect : "/login",
+    successRedirect : "/",
+    failureFlash : true
+}))
 
+app.post("/register" , async (req, res) => {
 
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id : Date.now().toString(),
+            name : req.body.name,
+            email : req.body.email,
+            password : hashedPassword
+        });
 
+        console.log(users)
 
-
-
-
-
+        return res.redirect("/login");
+    
+       } catch (error) {
+        return res.redirect("/register");
+       } 
+})
 
 app.listen(3000, function(){
     console.log("Server is live !!!!!");
